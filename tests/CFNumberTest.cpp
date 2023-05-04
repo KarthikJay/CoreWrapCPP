b47ba3851f7aa4103f906ce91a0f09120422d7f8
Karthik Jayakumar <karthik_jayakumar@apple.com>
2023-05-04 09:00:26 -0700

    Library content update: CFNumber
    - Added default constructor to CFNumber (set value to 0 and size 8bit)
    - Templatized constructor for CFNumber
    - Added additional CFNumber tests

--- tests/CFNumberTest.cpp
+++ tests/CFNumberTest.cpp
@@ -7,18 +7,30 @@
 #include <boost/test/unit_test.hpp>
 
 BOOST_AUTO_TEST_SUITE(number_constructor)
-    BOOST_AUTO_TEST_CASE(default_integer_64bit_constructor)
+    BOOST_AUTO_TEST_CASE(default_constructor)
     {
         CF::Number test;
         BOOST_CHECK_EQUAL(test, (uint8_t)0);
         BOOST_CHECK_EQUAL(test, (int16_t)0);
         BOOST_CHECK_NE(test, 0.5f);
     }
-    BOOST_AUTO_TEST_CASE(integer_64bit_8bitmax_constructor)
+    BOOST_AUTO_TEST_CASE(integer_u8max_constructor)
     {
         uint64_t temp = std::numeric_limits<uint8_t>::max();
         CF::Number test(std::numeric_limits<uint8_t>::max());
         BOOST_CHECK_EQUAL(test, std::numeric_limits<uint8_t>::max());
         BOOST_CHECK_EQUAL(test, temp);
+    }
+    BOOST_AUTO_TEST_CASE(copy_float_constructor)
+    {
+        CF::Number test = 3.14159265;
+        BOOST_CHECK_EQUAL(test, 3.14159265);
+    }
+    BOOST_AUTO_TEST_CASE(copy_double_constructor)
+    {
+        float junk = 14.76273;
+        CF::Number test = std::numeric_limits<double>::max();
+        BOOST_CHECK_EQUAL(test, std::numeric_limits<double>::max());
+        BOOST_CHECK_NE(test, junk);
     }
 BOOST_AUTO_TEST_SUITE_END()
